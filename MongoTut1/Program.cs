a500a22ae4a977ebb1e553d74370069e2e16f21a
stevenelliottjr <stevene.jr@gmail.com>
2011-08-19 16:35:01 -0400

    got that shit yo

---
+++ MongoTut1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoTut1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            MongoServer server = MongoServer.Create();
+            MongoDatabase database = server.GetDatabase("books");
+            MongoCollection<BsonDocument> books =
+                database.GetCollection<BsonDocument>("books");
+            BsonDocument[] batch = {
+    new BsonDocument {
+        { "author", "Kurt Vonnegut" },
+        { "title", "Cat's Cradle" }
+    },
+    new BsonDocument {
+        { "author", "Kurt Vonnegut" },
+        { "title", "Slaughterhouse-Five" }
+    }
+};
+            books.InsertBatch(batch);
+
+        }
+    }
+}
